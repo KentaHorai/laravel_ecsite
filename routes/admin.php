12521ba69576da873ea6986d147912fb71796776
KentaHorai <chickentyan1023@gmail.com>
2023-02-17 10:23:09 +0900

    リソースコントローラ

--- routes/admin.php
+++ routes/admin.php
@@ -8,6 +8,7 @@ use App\Http\Controllers\Admin\Auth\NewPasswordController;
 use App\Http\Controllers\Admin\Auth\PasswordResetLinkController;
 use App\Http\Controllers\Admin\Auth\RegisteredUserController;
 use App\Http\Controllers\Admin\Auth\VerifyEmailController;
+use App\Http\Controllers\Admin\OwnersController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -25,10 +26,13 @@ Route::get('/', function () {
     return view('admin.welcome');
 });
 
+//http://127.0.0.1:8000/admin/owners
+Route::resource('owners',OwnersController::class)
+    ->middleware('auth:admin');//ログインしているかの認証(adminで認証されていたら表示)
+
 Route::get('/dashboard', function () {
     return view('admin.dashboard');
 })->middleware(['auth:admin'])->name('dashboard');
-
 
 Route::get('/register', [RegisteredUserController::class, 'create'])
                 ->middleware('guest')

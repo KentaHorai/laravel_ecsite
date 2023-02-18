cd7bce0fa5eab0ff53bcc085d24fb0a9c8b385e3
KentaHorai <chickentyan1023@gmail.com>
2023-02-19 00:23:26 +0900

    softDelete

--- app/Http/Controllers/Admin/OwnersController.php
+++ app/Http/Controllers/Admin/OwnersController.php
@@ -79,7 +79,8 @@ class OwnersController extends Controller
 
         return redirect()
         ->route('admin.owners.index')
-        ->with('message', 'オーナー登録を実施しました。');
+        ->with(['message' => 'オーナー登録を実施しました。',
+        'status' => 'info']);
     }
 
     /**
@@ -123,7 +124,8 @@ class OwnersController extends Controller
 
         return redirect()
         ->route('admin.owners.index')
-        ->with('message', 'オーナー情報を更新しました。');
+        ->with(['message' => 'オーナー情報を更新しました。',
+        'status' => 'info']);
     }
 
     /**
@@ -134,6 +136,21 @@ class OwnersController extends Controller
      */
     public function destroy($id)
     {
-        //
+        Owner::findOrFail($id)->delete();//ソフトデリート
+
+        return redirect()
+        ->route('admin.owners.index')
+        ->with(['message' => 'オーナー情報を削除しました。',
+        'status' => 'alert']);
+    }
+
+    public function expiredOwnerIndex(){
+        $expiredOwners = Owner::onlyTrashed()->get();//ソフトデリートしたユーザだけを取り出す
+        return view('admin.expired-owners', compact('expiredOwners'));
+    }
+    
+    public function expiredOwnerDestroy($id){
+        Owner::onlyTrashed()->findOrFail($id)->forceDelete();//完全に削除
+        return redirect()->route('admin.expired-owners.index'); 
     }
 }
